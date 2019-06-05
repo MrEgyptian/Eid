3bc80d95a076add46f6a20f3c40d45898a4d5e35
Ahmed Mohamed <44830370+Ahmedmahmed8a@users.noreply.github.com>
2019-06-05 08:17:44 +0200

    Delete eid.sh

--- eid.sh
+++
@@ -1,9 +0,0 @@
-cat .banner
-echo -e "\033[94m Do you wanna set it as a banner? [y/n]: "
-read option
-if $option =='y'
-then
-	cp -rf .banner $HOME
-	echo "cat .banner">>$HOME/.bashrc
-	echo "\033[92 The Banner Has been set 😊"
-fi
