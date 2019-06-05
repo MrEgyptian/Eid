0ab1642b5adb4db5a6871af3f62756291c72fb93
Ahmed Mohamed <44830370+Ahmedmahmed8a@users.noreply.github.com>
2019-06-05 08:18:58 +0200

    Add files via upload

---
+++ eid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $1 == 'install' ]
+then
+cp -rf .banner $HOME
+echo "cat .banner">>$HOME/.bashrc
+echo -e "\033[92m The Banner Has been set 😊"
+elif [ $1 == 'show' ]
+then
+cat .banner
+else
+echo You may not go to the party.
+fi
